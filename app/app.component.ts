a0043c2cb43a5b033d0de14f1ee3dce4c5b11df3
x159139 <ribadeau.baudoin-ext@socgen.com>
2017-01-16 17:02:39 +0100

    End of Part2: Selection

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,13 +22,15 @@ const HEROES: Hero[] = [
   template: `<h1>{{title}}</h1>
              <h2>My Heroes</h2>
              <ul class="heroes">
-               <li *ngFor='let hero of heroes' (click)='onSelect(hero)'>
+               <li *ngFor='let hero of heroes' (click)='onSelect(hero)' [class.selected]="hero === selectedHero" >
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
                </li>
              </ul>
-             <h2>{{hero.name}} details</h2>
-             <div><label>id: </label>{{hero.id}}</div>
-             <div><label>name: </label><input placeholder='name' [(ngModel)]='hero.name'/></div>`
+             <div *ngIf="selectedHero">
+               <h2>{{selectedHero.name}} details</h2>
+               <div><label>id: </label>{{selectedHero.id}}</div>
+               <div><label>name: </label><input placeholder='name' [(ngModel)]='selectedHero.name'/></div>
+             </div>`,
   styles: [`
    .selected { background-color: #CFD8DC !important; color: white; }
    .heroes { margin: 0 0 2em 0; list-style-type: none; padding: 0; width: 15em; }
@@ -42,6 +44,7 @@ const HEROES: Hero[] = [
 
 export class AppComponent  {
   title = 'Tour of Heroes';
-  hero: Hero = {id: 1, name: 'Windstorm'};
+  selectedHero: Hero;
   heroes = HEROES;
+  onSelect(hero: Hero): void { this.selectedHero = hero; };
 }
